e22d865dbf166b61f401a405845b7c9fcb467056
unknown <yshida1pc7@gmail.com>
2022-10-09 17:00:20 +0900

    upload : initial commit

---
+++ src/main/java/custommob/custommob/CustomMob.java
@@ -0,0 +1,17 @@
+package custommob.custommob;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class CustomMob extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
